4373d19bfc1f99eee709008a4f061a3237e7c7b3
Glugox <glugox@gmail.com>
2025-10-23 05:43:16 +0200

    Phpstan & Rector init

--- src/FileDeltaServiceProvider.php
+++ src/FileDeltaServiceProvider.php
@@ -6,12 +6,12 @@ use Illuminate\Support\ServiceProvider;
 
 class FileDeltaServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function register(): void
     {
         // Bind services or singletons here
     }
 
-    public function boot()
+    public function boot(): void
     {
         // Publish configs, migrations, routes if needed
     }
