1f5fb4c3d13dbb36e0ccaff9e0a414f6612583f1
Glugox <glugox@gmail.com>
2025-10-23 05:05:48 +0200

    first commit

---
+++ src/FileDeltaServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Glugox\FileDelta;
+
+use Illuminate\Support\ServiceProvider;
+
+class FileDeltaServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Bind services or singletons here
+    }
+
+    public function boot()
+    {
+        // Publish configs, migrations, routes if needed
+    }
+}
